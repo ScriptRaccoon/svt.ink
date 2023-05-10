aa45fed684f453b282c3251379bded36b8988b11
Script Raccoon <scriptraccoon@gmail.com>
2023-05-10 22:19:47 +0200

    add proper favicons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,22 +3,6 @@
 	import Header from "$lib/components/Header.svelte";
 </script>
 
-<svelte:head>
-	<title>Svt.Ink</title>
-	<meta property="og:title" content="Svt.Ink" />
-	<meta
-		property="og:description"
-		content="Yet another tool to create short URLs."
-	/>
-	<meta
-		name="description"
-		content="Yet another tool to create short URLs."
-	/>
-	<meta property="og:type" content="Website" />
-	<meta property="og:url" content="https://svt.ink" />
-	<meta property="og:site_name" content="Svt.Ink" />
-</svelte:head>
-
 <Header />
 
 <main>
