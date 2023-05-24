8db03d0f9c9e15fc784c105ff7e07869c0e37074
Script Raccoon <scriptraccoon@gmail.com>
2023-05-24 23:20:00 +0200

    improve styling

--- src/lib/components/Loader.svelte
+++ src/lib/components/Loader.svelte
@@ -1,4 +1,4 @@
-<div class="container">
+<div class="container" aria-label="loading...">
 	<div class="loader">
 		<div />
 		<div />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { slide, fade } from "svelte/transition";
 	import { enhance } from "$app/forms";
 	import { page } from "$app/stores";
 	import Loader from "$lib/components/Loader.svelte";
@@ -6,14 +7,16 @@
 
 	export let form: ActionData;
 
-	$: short_url = `${$page.url.origin}/${form?.shortcut}`;
+	const transition_options = { duration: 150 };
 
 	let show_confirmation = false;
+
+	$: short_url = `${$page.url.origin}/${form?.shortcut}`;
 
 	async function copy_url() {
 		await window.navigator.clipboard.writeText(short_url);
 		show_confirmation = true;
-		setTimeout(() => (show_confirmation = false), 1200);
+		setTimeout(() => (show_confirmation = false), 1500);
 	}
 
 	let loading = false;
@@ -23,11 +26,11 @@
 	}
 </script>
 
-<p class="teaser">
+<div class="teaser">
 	Yet another tool to create short URLs. Notice that this is a hobby
 	project. We do not guarantee that the links will be stored
 	forever.
-</p>
+</div>
 
 <form
 	class="form"
@@ -35,116 +38,102 @@
 	autocomplete="off"
 	use:enhance
 	on:submit={() => (loading = true)}
+	class:loading
 >
-	<p>
-		<label
-			>URL<input
-				type="text"
-				name="url"
-				value={form?.url ?? ""}
-			/>
-		</label>
-	</p>
-	<p>
-		<button>Create short URL</button>
-	</p>
+	<div>
+		<label for="url">URL</label>
+		<input
+			type="text"
+			id="url"
+			name="url"
+			value={form?.url ?? ""}
+			disabled={loading}
+		/>
+	</div>
+	<div>
+		<button disabled={loading}>Create short URL</button>
+	</div>
 </form>
 
-{#if loading}
-	<Loader />
-{/if}
+<div aria-live="polite">
+	{#if loading}
+		<Loader />
+	{:else if form?.errors}
+		<section
+			in:fade={transition_options}
+			aria-label="errors"
+			class="error"
+		>
+			{#each form.errors as error}
+				<div>{error}</div>
+			{/each}
+		</section>
+	{:else if form?.shortcut}
+		<section
+			in:fade={transition_options}
+			aria-label="result"
+			class="result"
+		>
+			<div>Success! The short URL is:</div>
 
-<div aria-live="assertive" class="error">
-	{#each form?.errors ?? [] as error}
-		<p>{error}</p>
-	{/each}
-</div>
-
-<div aria-live="polite" class="result">
-	{#if form?.shortcut}
-		<div>Success! The short URL is:</div>
-		<p>
-			<a href={short_url}>
-				{short_url}
-			</a>
-		</p>
-
-		<div>
-			<button on:click={copy_url}> Copy </button>
-		</div>
-
-		{#if show_confirmation}
-			<div class="confirmation">
-				Copied short URL to clipboard!
+			<div>
+				<a href={short_url}>
+					{short_url}
+				</a>
 			</div>
-		{/if}
+
+			<div class="copy">
+				<button on:click={copy_url}>Copy</button>
+
+				{#if show_confirmation}
+					<div
+						class="confirmation"
+						transition:slide={transition_options}
+					>
+						Copied short URL to clipboard!
+					</div>
+				{/if}
+			</div>
+		</section>
 	{/if}
 </div>
 
 <style>
 	.teaser {
-		font-size: 1rem;
 		color: var(--dark-font-color);
-	}
-
-	button,
-	input {
-		font-family: inherit;
-		font-size: inherit;
-		border: none;
-		color: inherit;
-		border-radius: 0.25rem;
-	}
-
-	button {
-		background-color: var(--accent-color);
-		padding: 0.25rem 0.6rem;
-		cursor: pointer;
-	}
-
-	button:focus-visible {
-		outline: 1px solid var(--font-color);
-	}
-
-	button:disabled,
-	input:disabled {
-		cursor: not-allowed;
-	}
-
-	input {
-		padding: 0.4rem;
-		display: block;
-		width: 100%;
-		background-color: var(--font-color);
-		color: var(--bg-color);
-		margin-block: 0.2rem;
-	}
-
-	input:focus-visible {
-		outline: 1px solid var(--font-color);
-		outline-offset: 1px;
 	}
 
 	.form {
 		font-size: 1.25rem;
-		margin-top: 1rem;
+		margin-block: 1rem;
+		display: flex;
+		flex-direction: column;
+		gap: 0.75rem;
+		transition: opacity 200ms linear;
+	}
+
+	.form.loading {
+		opacity: 0.75;
+	}
+
+	.result {
+		margin-top: 2rem;
+		font-size: 1.25rem;
 	}
 
 	.error {
 		color: var(--error-color);
+		display: flex;
+		flex-direction: column;
+		gap: 0.5rem;
 	}
 
-	.result {
-		font-size: 1.25rem;
-		padding-top: 1.5rem;
-	}
-
-	p {
-		margin-bottom: 1rem;
+	.copy {
+		margin-top: 1rem;
 	}
 
 	.confirmation {
-		font-size: 1rem;
 		margin-top: 0.25rem;
+		color: var(--dark-font-color);
 	}
 </style>
